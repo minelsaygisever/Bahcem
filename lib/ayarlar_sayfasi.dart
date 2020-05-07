ed80ab651093a3b05b1ba275a472697326754af6
esra-polat <esrapolat1293@gmail.com>
2020-05-07 15:02:50 +0300

    edit

--- lib/ayarlar_sayfasi.dart
+++ lib/ayarlar_sayfasi.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'SizeConfig.dart';
 import 'custom_switch.dart';
-import 'hakkında_sayfasi.dart';
+import 'hakkinda_sayfasi.dart';
 
 class AyarlarSayfasi extends StatefulWidget {
   @override
