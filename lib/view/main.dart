c6f621015db73bf968b040b4cb822c50acbda588
esra-polat <esrapolat1293@gmail.com>
2020-05-26 17:48:22 +0300

    user_model düzenlendi

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<UserModel>.value(
+    return StreamProvider<AuthModel>.value(
       value: AuthService().user,
       child: MaterialApp(
         title: 'Bahçem',
