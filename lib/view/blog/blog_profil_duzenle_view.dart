e01e28456463bf98c5cdbe5ab15c2ba0b00b2f69
esra-polat <esrapolat1293@gmail.com>
2020-06-11 11:30:23 +0300

    bahcem düzenle sayfasındaki hatalar giderildi

--- lib/view/blog/blog_profil_duzenle_view.dart
+++ lib/view/blog/blog_profil_duzenle_view.dart
@@ -9,6 +9,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:scheduled_notifications/scheduled_notifications.dart';
 
 class BlogProfilDuzenle extends StatefulWidget {
   final String blogIsimDetail;
@@ -41,6 +42,7 @@ class _BlogProfilDuzenleState extends State<BlogProfilDuzenle> {
   int count_isim = 0;
   int count_bio = 0;
   int count_profilImg = 0;
+
 
   @override
   void initState() {
@@ -214,6 +216,8 @@ class _BlogProfilDuzenleState extends State<BlogProfilDuzenle> {
                                     ? widget.profilImgDetail
                                     : _profilImgUrl,
                                 widget.indexDetail);
+                            _bildirim();
+
                             setState(() {
                               Navigator.pop(context);
                             });
